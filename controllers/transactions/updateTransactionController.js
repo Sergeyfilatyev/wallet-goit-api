c8c75fce779bda3b568450630fd182ee288595cc
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-04-07 00:41:46 +0300

    enum validation fixed

--- controllers/transactions/updateTransactionController.js
+++ controllers/transactions/updateTransactionController.js
@@ -3,15 +3,20 @@ const { RequestError } = require("../../helpers");
 
 const updateTransactionController = async (req, res) => {
   const { transactionId: id } = req.params;
-  const updatedTransaction = await Transaction.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
+    const updatedTransaction = await Transaction.findByIdAndUpdate(
+      id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedTransaction) {
+      throw RequestError(404, "Not found!");
+    }
 
-  if (!updatedTransaction) {
-    throw RequestError(404, "Not found!");
-  }
+    return res.status(200).json(updatedTransaction);
 
-  return res.status(200).json(updatedTransaction);
 };
 
 module.exports = updateTransactionController;
