bc598456f1e8a24bdbffa39b44d5ecb94ca540d1
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2023-04-06 10:42:56 +0200

    Upload users-register

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const usersRoutes = require("./routes/api/usersRoutes");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -8,6 +9,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
