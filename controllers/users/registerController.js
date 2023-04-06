542d9b8e57b514a843b3a9331b3bbcce8257b8b1
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2023-04-06 12:35:01 +0200

    Uploads registerControler

--- controllers/users/registerController.js
+++ controllers/users/registerController.js
@@ -1,4 +1,22 @@
-const { User } = require("../../models/user");
+const { User } = require("../../models");
+const { RequestError } = require("../../helpers");
 const bcrypt = require("bcryptjs");
-const registerContoller = () => {};
+
+const registerContoller = async (req, res) => {
+  const { name, password, email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, "Email in use!");
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ name, password: hashPassword, email });
+  if (!newUser) {
+    res.status(400);
+    throw new Error("Cannot save user!");
+  }
+  res.status(201).json({
+    name: newUser.name,
+    email: newUser.email,
+  });
+};
 module.exports = registerContoller;
