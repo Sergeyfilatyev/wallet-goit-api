669865affe7b5bd8d61b41341ad4ae4a73cd3187
olga-zak <olgaliam@gmail.com>
2023-04-06 21:03:34 +0300

    add transaction joiSchemas

---
+++ schemas/transaction.js
@@ -0,0 +1,38 @@
+const Joi = require("joi");
+
+const addIncomeTransaction = Joi.object({
+  type: Joi.boolean().required(), //есть ли смысл ставить по умолчанию true(income)?
+  amount: Joi.number().required(),
+  category: Joi.string(),
+  comment: Joi.string(),
+  date: Joi.date().required(),
+});
+
+const addExpenceTransaction = Joi.object({
+  type: Joi.boolean().required(), //есть ли смысл ставить по умолчанию true(income)?
+  amount: Joi.number().required(),
+  category: Joi.string().required(), //add existing categories
+  comment: Joi.string(),
+  date: Joi.date().required(),
+});
+
+const editTransaction = Joi.object({
+  id: Joi.string.required(),
+  type: Joi.boolean(), //есть ли смысл ставить по умолчанию true(income)?
+  amount: Joi.number(),
+  category: Joi.string(), //add existing categories
+  comment: Joi.string(),
+  date: Joi.date(),
+});
+//at least one of the fields should be edited
+
+const deleteTransaction = Joi.object({
+  id: Joi.string.required(),
+});
+
+module.exports = {
+  addIncomeTransaction,
+  addExpenceTransaction,
+  editTransaction,
+  deleteTransaction,
+};
