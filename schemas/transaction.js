dcd4e38a04d203272b626b879fe2ddc9dba42d49
olga-zak <olgaliam@gmail.com>
2023-04-08 01:20:38 +0300

    remove unnecessary schemas

--- schemas/transaction.js
+++ schemas/transaction.js
@@ -1,23 +1,14 @@
 const Joi = require("joi");
 
-const addIncomeTransaction = Joi.object({
-  type: Joi.boolean().required(),
-  amount: Joi.number().required(),
-  category: Joi.string(),
-  comment: Joi.string(),
-  date: Joi.date().required(),
-});
-
-const addExpenceTransaction = Joi.object({
+const addTransaction = Joi.object({
   type: Joi.boolean().required(),
   amount: Joi.number().required(),
   category: Joi.string().required(), //add existing categories
   comment: Joi.string(),
-  date: Joi.date().required(),
+  //date: Joi.date().required(),
 });
 
 const editTransaction = Joi.object({
-  id: Joi.string().required(),
   type: Joi.boolean(),
   amount: Joi.number(),
   category: Joi.string(), //add existing categories
@@ -26,13 +17,7 @@ const editTransaction = Joi.object({
 });
 //at least one of the fields should be edited
 
-const deleteTransaction = Joi.object({
-  id: Joi.string().required(),
-});
-
 module.exports = {
-  addIncomeTransaction,
-  addExpenceTransaction,
+  addTransaction,
   editTransaction,
-  deleteTransaction,
 };
