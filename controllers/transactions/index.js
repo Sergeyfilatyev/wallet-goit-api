0448a2fd139425ac7b187340a0757565cce13f6e
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-07 20:25:05 +0300

    corrected

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -2,9 +2,7 @@ const addTransactionController = require("./addTransactionController");
 const updateTransactionController = require("./updateTransactionController");
 const deleteTransactionController = require("./deleteTransactionController");
 const getTransactionController = require("./getTransactionController");
-const getCategoryTransactionController = require("./getCategoryTransactionController");
 const getAllTransactionsController = require("./getAllTransactionsController");
-
 
 module.exports = {
   getAllTransactionsController,
