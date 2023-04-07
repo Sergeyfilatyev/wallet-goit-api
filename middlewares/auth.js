34b22f71f2cafea9764472ca231d52f964b18b26
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-04-07 22:56:56 +0300

    fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,12 +10,13 @@ const auth = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") {
-    throw RequestError(401);
-  }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
+
+    if (bearer !== "Bearer") {
+      throw RequestError(401);
+    }
 
     if (!user || !user.token) {
       throw RequestError(401);
