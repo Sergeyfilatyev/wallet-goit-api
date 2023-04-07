e09845a47a9badbf4b9b9a3b964869e0034d9158
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-07 12:08:58 +0300

    corrected-category-transaction

--- routes/api/transactionsRouter.js
+++ routes/api/transactionsRouter.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const controllerWrapper = require("../../helpers/controllerWrapper");
-const { addTransactionController, updateTransactionController, deleteTransactionController, getTransactionController,
-    getCategoryTransactionController} = require("../../controllers");
+const {
+  addTransactionController,
+  updateTransactionController,
+  deleteTransactionController,
+  getTransactionController,
+  getCategoryTransactionController,
+} = require("../../controllers");
 
 const transactionRouter = express.Router();
 
 transactionRouter.post("/", controllerWrapper(addTransactionController));
-transactionRouter.patch("/:transactionId", controllerWrapper(updateTransactionController));
-transactionRouter.delete("/:transactionId", controllerWrapper(deleteTransactionController));
-transactionRouter.get("/:transactionId", controllerWrapper(getTransactionController))
-transactionRouter.get("/:transactionId", controllerWrapper(getCategoryTransactionController))
+transactionRouter.patch(
+  "/:transactionId",
+  controllerWrapper(updateTransactionController)
+);
+transactionRouter.delete(
+  "/:transactionId",
+  controllerWrapper(deleteTransactionController)
+);
+transactionRouter.get(
+  "/:transactionId",
+  controllerWrapper(getTransactionController)
+);
+transactionRouter.get(
+  "/category/:transactionId",
+  controllerWrapper(getCategoryTransactionController)
+);
 
 module.exports = transactionRouter;
