58cb0fa670f8a467da8c16215fef0a0cf5d0e34c
olga-zak <olgaliam@gmail.com>
2023-04-08 00:47:54 +0300

    add register and login validation
    commented 2nd router.get /current

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -7,23 +7,29 @@ const {
   getCurrentUserController,
 } = require("../../controllers/");
 const controllerWrapper = require("../../helpers/controllerWrapper");
+const { validateBody } = require("../../helpers");
+const { registerUserSchema, loginUserSchema } = require("../../schemas");
 const { auth } = require("../../middlewares");
 
-router.post("/register", controllerWrapper(registerController));
-router.post("/login", controllerWrapper(loginController));
-
-
-router.get(
-  "/current",
-  authentificate,
-  controllerWrapper(getCurrentUserController)
+router.post(
+  "/register",
+  validateBody(registerUserSchema),
+  controllerWrapper(registerController)
 );
+router.post(
+  "/login",
+  validateBody(loginUserSchema),
+  controllerWrapper(loginController)
+);
+
+// router.get(
+//   "/current",
+//   // authentificate,
+//   controllerWrapper(getCurrentUserController)
+// );
 
 router.get("/logout", auth, controllerWrapper(logoutController));
 
-router.get("/current", auth, controllerWrapper(getCurrentUser));
-
-
-
+router.get("/current", auth, controllerWrapper(getCurrentUserController));
 
 module.exports = router;
