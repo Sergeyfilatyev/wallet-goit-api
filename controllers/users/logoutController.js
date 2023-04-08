420f5d164aa708e65cd6e6300b2cfa11d3ee6ce5
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-08 11:32:12 +0300

    swagger-in-progress

--- controllers/users/logoutController.js
+++ controllers/users/logoutController.js
@@ -5,37 +5,9 @@ const logoutController = async (req, res) => {
 
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(200).json({
+  res.status(204).json({
     message: "Logout successful",
   });
-  // const logout = async (id) => {
-  //   const user = await User.findOne({ id });
-
-  //   if (!user) {
-  //     return res.status(401).json({
-  //       status: "error",
-  //       code: 401,
-  //       message: "Not authorized",
-  //     });
-  //   }
-  //   try {
-  //     user.token = null;
-  //     await user.save();
-
-  //     return res.status(202).json({
-  //       message: "Logout successful",
-  //     });
-  //   } catch (err) {
-  //     console.error(err.message);
-  //     return res.status(500).json({
-  //       status: "error",
-  //       code: 500,
-  //       message: "Internal server error",
-  //     });
-  //   }
-  // };
-
-  // logout(id);
 };
 
 module.exports = logoutController;
