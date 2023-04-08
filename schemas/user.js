901717abeada24c2ffd0a6b1bfc98119eab4a1ae
olga-zak <olgaliam@gmail.com>
2023-04-08 13:23:23 +0300

    update pass regex

--- schemas/user.js
+++ schemas/user.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const emailRegex =
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const passRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?.,\/\-_&])[A-Za-z\d@$!%*?.,\/\-_&]{8,}$/; //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
 
 const registerUserSchema = Joi.object({
   email: Joi.string().pattern(emailRegex).required(),
