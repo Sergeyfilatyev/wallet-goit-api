5dac0368a2c56810ddd6d6fd532fec5f7f37b4c4
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-04-08 21:22:58 +0300

    balance counting fixed

--- controllers/transactions/updateTransactionController.js
+++ controllers/transactions/updateTransactionController.js
@@ -19,15 +19,46 @@ const updateTransactionController = async (req, res) => {
     throw RequestError(404, "Not found!");
   }
 
-  if (req.body.amount) {
+  if (req.body.amount || "income" in req.body) {
     const user = await User.findOne(owner);
-    const calculatedNumber =
-      transactionToUpdate.income === true
-        ? updatedTransaction.amount - transactionToUpdate.amount
-        : transactionToUpdate.amount - updatedTransaction.amount;
 
-    user.balance = user.balance + calculatedNumber;
-    user.save();
+    if (req.body.amount && !("income" in req.body)) {
+      const calculatedNumber =
+        transactionToUpdate.income === true
+          ? updatedTransaction.amount - transactionToUpdate.amount
+          : transactionToUpdate.amount - updatedTransaction.amount;
+
+      user.balance = user.balance + calculatedNumber;
+      user.save();
+    }
+
+    if ("income" in req.body && !req.body.amount) {
+      calculatedNumber =
+        transactionToUpdate.income === true &&
+        updatedTransaction.income === false
+          ? transactionToUpdate.amount * -2
+          : transactionToUpdate.income === false &&
+            updatedTransaction.income === true
+          ? transactionToUpdate.amount * 2
+          : 0;
+
+      user.balance = user.balance + calculatedNumber;
+      user.save();
+    }
+
+    if ("income" in req.body && req.body.amount) {
+      const calculatedNumber =
+        transactionToUpdate.income === true &&
+        updatedTransaction.income === false
+          ? (updatedTransaction.amount + transactionToUpdate.amount) * -1
+          : transactionToUpdate.income === false &&
+            updatedTransaction.income === true
+          ? transactionToUpdate.amount + updatedTransaction.amount
+          : 0;
+
+      user.balance = user.balance + calculatedNumber;
+      user.save();
+    }
   }
 
   return res.status(200).json(updatedTransaction);
