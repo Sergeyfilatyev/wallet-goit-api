a8c1e1c079fab2e2e8de6477bfac8f34e586c787
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-04-10 15:42:29 +0300

    refresh token partially done

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const logoutController = require("./logoutController");
 const registerController = require("./registerController");
 const loginController = require("./loginController");
 const getCurrentUserController = require("./getCurrentUserController");
+const refreshController = require("./refreshController");
 
 module.exports = {
   logoutController,
   registerController,
   loginController,
   getCurrentUserController,
+  refreshController,
 };
