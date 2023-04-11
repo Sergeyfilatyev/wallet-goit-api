2ceb8ac6992294584b898e8fa7b1f2ea32c8b204
Anastassia Vomm <anastassia.vomm@gmail.com>
2023-04-11 19:49:24 +0300

    balance and registration fixed

--- controllers/transactions/getAllTransactionsController.js
+++ controllers/transactions/getAllTransactionsController.js
@@ -1,14 +1,17 @@
-const { Transaction } = require("../../models");
+const { Transaction, User } = require("../../models");
 const { RequestError } = require("../../helpers");
 
 const getAllTransactionsController = async (req, res) => {
   const { _id: owner } = req.user;
   const allTransactions = await Transaction.find({ owner: owner });
 
+  const user = await User.findById(owner);
+
   if (!allTransactions) {
     throw RequestError(404, "Not found");
   }
-  return res.status(200).json({ data: allTransactions });
+
+  return res.status(200).json({ data: allTransactions, balance:  user.balance});
 };
 
 module.exports = getAllTransactionsController;
